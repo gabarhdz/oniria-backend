3bcc7841abdae85e9bd81e39d4018d8311319f01
gabarhdz <gabarhdz@outlook.com>
2025-05-14 08:47:42 -0600

    add profile_pic to psychologist

--- apps/psychologists/serializers.py
+++ apps/psychologists/serializers.py
@@ -7,7 +7,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name','profile_pic']
 
 class UniversitySerializer(serializers.ModelSerializer):
     class Meta:
