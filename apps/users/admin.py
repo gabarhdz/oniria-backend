c027a7ef36b05f5763d7e107081e0bfca7d84dbc
Gabriel Abarca <epoabarca@gmail.com>
2025-05-21 23:18:02 -0600

    changed admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import User,SleepState
+from unfold.admin import ModelAdmin as unfoldModelAdmin
 # Register your models here.
 admin.site.register(User)
 admin.site.register(SleepState)
+class ModelAdmin(unfoldModelAdmin):
+    pass
