73baef40057289be62da27f72993bd8b98f954f3
Gabarhdz <gabarhdz@outlook.com>
2025-06-09 13:47:23 -0600

    urls

--- backendExpo/urls.py
+++ backendExpo/urls.py
@@ -27,3 +27,7 @@ urlpatterns = [
     path('auth/',include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),    
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
