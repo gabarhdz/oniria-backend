75d1c5b4b5e5cf33b5bd474861f68d1b28dfa2a8
Gabarhdz <gabarhdz@outlook.com>
2025-06-09 09:33:31 -0600

    change in db

--- apps/community/serializers.py
+++ apps/community/serializers.py
@@ -1,4 +1,12 @@
-# serializers.py generado autom�ticamente
-
 from rest_framework import serializers
+from .models import Community
 
+
+class CommunitySerializer(serializers.Serializer):
+    class Meta:
+        model = Community
+        fields = ['url', 'id', 'name', 'description', 'profile_pic','created_at']
+        extra_kwargs = {
+            'url': {'view_name': 'community-detail', 'lookup_field': 'pk'},
+        }
+    
--- apps/community/views.py
+++ apps/community/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Community
+from .serializers import CommunitySerializer
 # Create your views here.
+class Communities(APIView):
+    def get(self, request, *args, **kwargs):
+        """
+        Retrieve all communities.
+        """
+        communities = Community.objects.all()
+        serializer = CommunitySerializer(communities, many=True)
+        return Response(serializer.data)
