ab215e82e5890dae8959791b0df16aea383df20a
Gabarhdz <gabarhdz@outlook.com>
2025-06-11 10:15:56 -0600

    Made regex to admit only strong passwords

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,16 +1,28 @@
-# serializers.py generado autom�ticamente
-
 from rest_framework import serializers
 from .models import User
+import re
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['url','id','username','email','SleepState','is_psychologist','description','profile_pic','password']
+        fields = ['url', 'id', 'username', 'email', 'SleepState', 'is_psychologist', 'description', 'profile_pic', 'password']
         extra_kwargs = {
             'url': {'view_name': 'user-detail', 'lookup_field': 'pk'},
         }
+
+    def validate_password(self, value):
+        # Si el regex NO coincide, lanza ValidationError
+        if not re.search(
+            r'^(?=(.*[A-Z]){1,})(?=(.*\d){3,})(?=(.*[!@#$%^&*()_+\-=\[\]{}|;:\'",.<>\/?]){1,}).{12,}$',
+            value
+        ):
+            raise serializers.ValidationError(
+                "La contraseña debe tener al menos 12 caracteres, 1 mayúscula, 3 números y un carácter especial."
+            )
+        return value  # Si pasa la validación, devuelve el valor
+
     def create(self, validated_data):
         user = User.objects.create_user(
             username=validated_data['username'],
@@ -18,7 +30,8 @@ class UserSerializer(serializers.ModelSerializer):
             description=validated_data['description'],
             profile_pic=validated_data['profile_pic']
         )
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data['password'])  # La validación ya ocurrió en validate_password
         user.save()
         return user
+    
 
