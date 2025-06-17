2cd5fe4ea976d65cd2eb231d838b7e8f7bd0e7ff
Gabriel Abarca Hernández <gabarhdz@outlook.com>
2025-06-17 17:52:46 -0600

    changes

--- apps/community/views.py
+++ apps/community/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Community
 from .serializers import CommunitySerializer
 # Create your views here.
@@ -14,6 +15,7 @@ class Communities(APIView):
         print(serializer.data)
         return Response(serializer.data)
     def post(self, request, *args, **kwargs):
+        permission_classes = [IsAuthenticated]
         """
         Create a new community.
         """
