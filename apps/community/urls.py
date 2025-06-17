8ce5b5e31a02c7461a741405ed336e61bec10693
Gabriel Abarca Hernández <gabarhdz@outlook.com>
2025-06-16 21:55:59 -0600

    able to see specific communities

--- apps/community/urls.py
+++ apps/community/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Communities,SimilarCommunities
+from .views import Communities,SimilarCommunities,DetailedCommunity
 
 urlpatterns = [
-    path('', Communities.as_view(), name='communities'),
-    path('<str:name>/',SimilarCommunities.as_view(),name="get-similar-communities")
+    path('<str:name>/',SimilarCommunities.as_view(),name="get-similar-communities"),
+    path('specific/<str:pk>/', DetailedCommunity.as_view(),name='specific community'),
+    path('', Communities.as_view(), name='communities')
+    
 ]
