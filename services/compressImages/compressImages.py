91540ae9640f02bebbf7813287a4aff1e3c7cfe5
Gabriel Abarca Hernández <gabarhdz@outlook.com>
2025-07-28 13:43:41 -0600

    fix functions

--- services/compressImages/compressImages.py
+++ services/compressImages/compressImages.py
@@ -1,5 +1,9 @@
 from django.utils.deconstruct import deconstructible
 from PIL import Image
 @deconstructible
-def compressImages():
-    pass
+class compressImages:
+    def __call__(self, image_path):
+        img = Image.open(image_path)
+        img = img.convert('RGB')  
+        img.save(image_path, format='JPEG', quality=45, optimize=True)
+
