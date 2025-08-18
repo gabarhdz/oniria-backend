597a37ea8b73a6eb20ba9f29aeceacfb283aef3d
gabarhdz <gabarhdz@outlook.com>
2025-08-17 21:47:17 -0600

    build view

--- apps/dreams/views.py
+++ apps/dreams/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from services.aiImplementation.deepseek_basic_call import deepseek_basic_call
 # Create your views here.
 class AI_basic_call:
     def post(self,request,*args,**kwargs):
+        data = request.data
+        user = request.user
+        response = deepseek_basic_call(data.prompt)
+        return(response)
