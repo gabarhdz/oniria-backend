88801524aa052305fea72a4df76b3cb527b9f96b
gabarhdz <gabarhdz@outlook.com>
2025-08-17 22:46:34 -0600

    fix AI_basic_call

--- apps/dreams/views.py
+++ apps/dreams/views.py
@@ -3,8 +3,9 @@ from rest_framework.views import APIView
 from services.aiImplementation.deepseek_basic_call import deepseek_basic_call
 # Create your views here.
 class AI_basic_call(APIView):
-    def post(self,request,*args,**kwargs):
+    def post(self, request, *args, **kwargs):
         data = request.data
         user = request.user
-        response = deepseek_basic_call(data.prompt)
-        return(response)
+        prompt = data.get("prompt")   # <- Aquí el cambio
+        response = deepseek_basic_call(prompt)
+        return Response({"result": response})
