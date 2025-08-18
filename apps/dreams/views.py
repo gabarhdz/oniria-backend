edaeac5d8cbf1c99a1ce290c087678cf88df66f7
gabarhdz <gabarhdz@outlook.com>
2025-08-17 22:56:48 -0600

    final fix to the view

--- apps/dreams/views.py
+++ apps/dreams/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
 from services.aiImplementation.deepseek_basic_call import deepseek_basic_call
 # Create your views here.
+
 class AI_basic_call(APIView):
     def post(self, request, *args, **kwargs):
         data = request.data
         user = request.user
-        prompt = data.get("prompt")   # <- Aquí el cambio
-        response = deepseek_basic_call(prompt)
-        return Response({"result": response})
+        prompt = data.get("prompt")
+
+        deepseek = deepseek_basic_call()
+        result = deepseek(prompt)
+
+        return Response({"result": result})
+
