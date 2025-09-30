746a2d3c0b221e0982684cac4cec29c29052230e
gabarhdz <gabarhdz@outlook.com>
2025-09-30 10:04:42 -0600

    fix .env issues

--- services/splitPDF/splitPDF.py
+++ services/splitPDF/splitPDF.py
@@ -3,30 +3,33 @@ from langchain_community.embeddings.sentence_transformer import SentenceTransfor
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 import os, chromadb
-from chromadb.config import Settings
+from decouple import config
+from dotenv import load_dotenv
 
-
+load_dotenv()
 
 # 1. Load documento
-loader = PyPDFLoader("./pdfs/Rutinas de sueño.pdf")
+loader = PyPDFLoader("./pdfs/TCC.pdf")
 document = loader.load()
 
 # 2. Break in chunks
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=0) 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=12)
 chunked_documents = text_splitter.split_documents(document)
+print(f"Chunked doc: {chunked_documents}")
 
 # 3. Embeddings
 embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
-# 4. Client Chroma Cloud
-chroma_client = chromadb.Client(
-    Settings(
-        chroma_api_impl="chromadb.api.client.CloudClient",
-        chroma_server_ssl=True,
-        chroma_api_key=os.getenv("CHROMA_API_KEY"),
-        tenant=os.getenv("CHROMA_TENANT"),
-        database=os.getenv("CHROMA_DATABASE"),
-    )
+# 4. Client Chroma Cloud - USA HttpClient en lugar de Client
+chroma_client = chromadb.HttpClient(
+    host=config("CHROMADB_HOST"),  # Por ejemplo: "api.trychroma.com"
+    port=443,  # Puerto SSL
+    ssl=True,
+    headers={
+        "x-chroma-token": config("CHROMADB_API_KEY")
+    },
+    tenant=config("CHROMADB_TENANT"),
+    database=config("CHROMADB_DATABASE"),
 )
 
 # 5. LangChain-Chroma wrapper
@@ -36,5 +39,6 @@ vectorstore = Chroma(
     embedding_function=embedding_function,
 )
 
-# 6. Addd documentos
+# 6. Add documentos
 vectorstore.add_documents(chunked_documents)
+print(f" {len(chunked_documents)} documentos agregados exitosamente")
