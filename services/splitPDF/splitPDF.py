536ea17fd4f6a89e33af0e9c8de07eb00e3d837e
gabarhdz <gabarhdz@outlook.com>
2025-09-30 10:36:46 -0600

    made the funciton deconstructible

--- services/splitPDF/splitPDF.py
+++ services/splitPDF/splitPDF.py
@@ -5,40 +5,44 @@ from langchain_community.document_loaders import PyPDFLoader
 import os, chromadb
 from decouple import config
 from dotenv import load_dotenv
-
+from django.utils.deconstruct import deconstructible
+# set dotenv
 load_dotenv()
 
-# 1. Load documento
-loader = PyPDFLoader("./pdfs/TCC.pdf")
-document = loader.load()
+@deconstructible
+class splitPDF:
+    def __call__(self):
+        # 1. Load document
+        loader = PyPDFLoader("./pdfs/Psicoanalisis.pdf")
+        document = loader.load()
 
-# 2. Break in chunks
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=12)
-chunked_documents = text_splitter.split_documents(document)
-print(f"Chunked doc: {chunked_documents}")
+        # 2. Break in chunks
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=800, chunk_overlap=12)
+        chunked_documents = text_splitter.split_documents(document)
+        print(f"Chunked doc: {chunked_documents}")
 
-# 3. Embeddings
-embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+        # 3. Embeddings
+        embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
-# 4. Client Chroma Cloud - USA HttpClient en lugar de Client
-chroma_client = chromadb.HttpClient(
-    host=config("CHROMADB_HOST"),  # Por ejemplo: "api.trychroma.com"
-    port=443,  # Puerto SSL
-    ssl=True,
-    headers={
-        "x-chroma-token": config("CHROMADB_API_KEY")
-    },
-    tenant=config("CHROMADB_TENANT"),
-    database=config("CHROMADB_DATABASE"),
-)
+        # 4. Client Chroma Cloud - USA HttpClient en lugar de Client
+        chroma_client = chromadb.HttpClient(
+            host=config("CHROMADB_HOST"),  # Por ejemplo: "api.trychroma.com"
+            port=443,  # Puerto SSL
+            ssl=True,
+            headers={
+                "x-chroma-token": config("CHROMADB_API_KEY")
+            },
+            tenant=config("CHROMADB_TENANT"),
+            database=config("CHROMADB_DATABASE"),
+        )
 
-# 5. LangChain-Chroma wrapper
-vectorstore = Chroma(
-    client=chroma_client,
-    collection_name="noctiria_knowledge",
-    embedding_function=embedding_function,
-)
+        # 5. LangChain-Chroma wrapper
+        vectorstore = Chroma(
+            client=chroma_client,
+            collection_name="noctiria_knowledge",
+            embedding_function=embedding_function,
+        )
 
-# 6. Add documentos
-vectorstore.add_documents(chunked_documents)
-print(f" {len(chunked_documents)} documentos agregados exitosamente")
+        # 6. Add documentos
+        vectorstore.add_documents(chunked_documents)
+        print(f" {len(chunked_documents)} documentos agregados exitosamente")
