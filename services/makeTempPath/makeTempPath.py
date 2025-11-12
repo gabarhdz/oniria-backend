ee2dfbb6c614cb2641bc0cf946772ce268a9ebc3
gabarhdz <gabarhdz@outlook.com>
2025-11-12 10:06:31 -0600

    fixes

--- services/makeTempPath/makeTempPath.py
+++ services/makeTempPath/makeTempPath.py
@@ -1,9 +1,22 @@
+import os
+import uuid
+import base64
+from datetime import datetime
 from django.utils.deconstruct import deconstructible
-from PIL import Image
+from ..compressImages.compressImages import compressImages
+
 @deconstructible
-class compressImages:
-    def __call__(self, image_path):
-        img = Image.open(image_path)
-        img = img.convert('RGB')  
-        img.save(image_path, format='JPEG', quality=45, optimize=True)
-        return image_path
+class makeTempPath:
+    def __init__(self, base_dir='tmp_uploads'):
+        self.base_dir = base_dir
+    
+    def __call__(self, instance, filename):
+        ext = filename.split('.')[-1]
+        path = os.path.join(
+            self.base_dir,
+            str(instance.id),
+            'images',
+            'profilepic'
+        )
+        
+        return os.path.join(path, f'{instance.id}.{ext}')
