b886bd32e4836e3db079275dcce21711fb720356
gabarhdz <gabarhdz@outlook.com>
2025-11-12 15:12:55 -0600

    get specific psychologists

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,6 +1,6 @@
 # apps/users/urls.py
 from django.urls import path
-from .views import getSpecificUser, getAllUsers, getCurrentUser,CreatePsychologists,AllPsychologists
+from .views import getSpecificUser, getAllUsers, getCurrentUser,CreatePsychologists,AllPsychologists,SpecificPsychologist
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', getAllUsers.as_view(), name='user-list'),
     path('me/', getCurrentUser.as_view(), name='current-user'),
     path('create-psychologist/',CreatePsychologists.as_view(),name='create-psychologist'), 
-    path('psychologist/',AllPsychologists.as_view(),name="get psychologists"), # Nueva ruta para usuario actual
+    path('psychologist/',AllPsychologists.as_view(),name="get psychologists"),
+    path('psychologist/<str:pk>/',SpecificPsychologist), # Nueva ruta para usuario actual
     path('<str:pk>/', getSpecificUser.as_view(), name='user-detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- apps/users/views.py
+++ apps/users/views.py
@@ -193,3 +193,14 @@ class AllPsychologists(APIView):
         psychologists = User.objects.filter(is_psychologist=True)
         serializer = UserSerializer(psychologists, many=True, context={'request': request})
         return Response(serializer.data)
+    
+class SpecificPsychologist(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, pk, *args, **kwargs):
+        """
+        Retrieve a psychologist user instance.
+        """
+        psychologist = get_object_or_404(User, pk=pk, is_psychologist=True)
+        serializer = UserSerializer(psychologist, context={'request': request})
+        return Response(serializer.data)
